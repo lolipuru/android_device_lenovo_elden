afe81404609580b4b28bd8e0369f7fa25ade0338
Roopesh Nataraja <quic_roopeshr@quicinc.com>
2023-05-13 15:28:41 +0000

    elden: Enable compilation of init_boot partition
    Devices launching with Android 13 or higher and shipping with
    Android 13 must have an init_boot static partition. The generic
    ramdisk is no longer be contained in the boot.img file that goes
    in the boot partition. This generic ramdisk will be placed in a
    new init_boot.img to be installed in a new init_boot partition.
    
    Change-Id: I0434ef0cee209bb7709474c6db15b2c4fcb40a03

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,6 +14,7 @@ AB_OTA_UPDATER := true
 
 AB_OTA_PARTITIONS += \
     boot \
+    init_boot \
     vendor_boot \
     dtbo \
     vbmeta \
@@ -57,6 +58,10 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_BOOTLOADER_BOARD_NAME := sun
 TARGET_NO_BOOTLOADER := true
 TARGET_USES_UEFI := true
+
+# Init Boot
+BOARD_INIT_BOOT_HEADER_VERSION := 4
+BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
 
 # Kernel
 BOARD_KERNEL_PAGESIZE   := 4096
