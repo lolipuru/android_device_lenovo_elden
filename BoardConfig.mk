a23d3d7aceb8094822932c9d5bb2c8237bf6770c
Karra Harshitha <quic_kharshit@quicinc.com>
2023-11-09 17:18:41 +0530

    elden: Enable QSPA
    Change-Id: If052ab21ab8fda0cc90b3fca352d1e75f423cb57

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,6 +80,7 @@ BOARD_BOOTCONFIG := \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     androidboot.load_modules_parallel=true \
+    androidboot.vendor.qspa=true \
     androidboot.selinux=permissive
 
 TARGET_NO_KERNEL_OVERRIDE := true
