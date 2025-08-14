970e40068147ecffb3a367ce394c4fb9193442dd
lolipuru <banstellazz@gmail.com>
2025-08-14 21:55:17 +0700

    elden: patch both c2.dolby.hevc.sec.dec and c2.dolby.hevc.dec with libcodec2_shim
    Change-Id: Id1d421f29fe974835cb3d9d27ef364ef2fcb0157
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -193,6 +193,11 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_lockPlanes')
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
+    (
+        'vendor/lib64/c2.dolby.hevc.sec.dec.so',
+        'vendor/lib64/c2.dolby.hevc.dec.so',
+    ): blob_fixup()
+        .add_needed('libcodec2_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
