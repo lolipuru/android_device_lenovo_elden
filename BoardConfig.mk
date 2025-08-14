24a1a80a2dd744af5c8334bf5957ec1e3aa64714
lolipuru <banstellazz@gmail.com>
2025-08-14 21:17:47 +0700

    elden: address FCM for device specific
    ERROR: files are incompatible: The following instances are in the device manifest but not specified in framework compatibility matrix:
        android.hardware.media.c2.IComponentStore/dolby (@1)
        vendor.dolby.dvs.IDvs/default (@1)
        vendor.qti.hardware.vibrator_dual.device.IVibratorDual/default (@1)
    
    Change-Id: I9ec2511eda44b0de29c37a43fa1ed67a3eca0fec
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,6 +185,7 @@ DEVICE_MANIFEST_SUN_FILES := \
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/compatibility_matrix.device.xml \
+    $(DEVICE_PATH)/configs/vintf/compatibility_matrix.lenovo.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MATRIX_FILE := \
