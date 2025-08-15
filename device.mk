1f60318a132a41127f2edde594ee34354409795c
lolipuru <banstellazz@gmail.com>
2025-08-16 00:02:48 +0700

    elden: re-import recovery init and ueventd from stock
    it have sligthly changes over qcom recovery init
    
    Change-Id: Ibbb6109e398a2b5c80c62ea98e70370034d323b3
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -226,6 +226,7 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc    
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/ueventd.recovery.rc:$(TARGET_COPY_OUT_RECOVERY)/root/system/ueventd.rc \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 # Sensors
