9ea5a76f7522f9ebe87c28902004e65e8cdfc032
lolipuru <banstellazz@gmail.com>
2025-08-24 14:08:14 +0700

    elden: build android.hardware.sensors-service.multihal from source
    Change-Id: Ib52019fb8dba81f42818d25ebedf6bc552d754d2
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -234,6 +234,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/ueventd.recovery.rc:$(TARGET_COPY_OUT_RECOVERY)/root/system/ueventd.rc \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors-service.multihal
+
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_sun/android.hardware.sensor.accelerometer.xml \
 	frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_sun/android.hardware.sensor.compass.xml \
